f3fb4007169630ce05f09990d92aa141b095be04
DeAngelisLuca <lucadeangelis1995@hotmail.it>
2017-04-18 19:58:44 +0200

    test aggiornamento solo locale

--- Prova/src/CiaoMondo.java
+++ Prova/src/CiaoMondo.java
@@ -3,6 +3,8 @@ public class CiaoMondo {
 	public static void main(String[] args) {
 		System.out.println("Ciao Mondo 2.0");
 		System.out.println("Test versione aggiornata");
+		System.out.println("Test versione aggiornata solo locale");
+
 	}
 
 }
